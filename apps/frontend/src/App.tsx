5952174bf9adf8d3cfb8f549122e5332f62a15f0
Shuvadeep Mondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-07-07 01:31:10 +0530

    feat:Routes (#2)

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import { Route, Routes} from "react-router-dom";
-import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/admin/Home";
+import MHome from "./components/main/MHome";
+import Login from "./components/admin/Login";
 function App() {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<MHome />} />
+        <Route path="/admin/home" element={<Home />} />
+        <Route path="/admin/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ apps/frontend/src/components/main/MHome.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function MHome() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to={'/admin/login'}>admin login</Link>
+      <Link to={'/admin/home'}>admin page</Link>
+    </div>
+  )
+}
+
+export default MHome
