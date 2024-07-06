e085993d3fc5a02e422f485b4021723ac6dd3975
Puskar-Roy <puskarroy600@gmail.com>
2024-07-07 03:07:08 +0530

    feat : fonts

--- apps/frontend/src/components/main/Navbar.tsx
+++ apps/frontend/src/components/main/Navbar.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-
+import '@repo/ui/main.css'
 const Navbar = () => {
   return (
     <header>
-      <nav>
-        <h1>fuddie .</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Download</li>
-          <li>Admin</li>
+      <nav className="flex justify-between w-[80%] mx-auto open-sans">
+        <h1 className="font-medium ">fuddie .</h1>
+        <ul className="flex items-center justify-center gap-10">
+          <li className="font-medium">Home</li>
+          <li className="font-medium">About</li>
+          <li className="font-medium">Download</li>
+          <li className="font-medium">Admin</li>
         </ul>
       </nav>
     </header>
