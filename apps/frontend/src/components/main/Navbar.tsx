d7ad83a2a33728feb47cdf8def5b5b940b2893ac
Puskar-Roy <puskarroy600@gmail.com>
2024-07-07 03:09:15 +0530

    feat : poppins

--- apps/frontend/src/components/main/Navbar.tsx
+++ apps/frontend/src/components/main/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
       <nav className="flex justify-between w-[80%] mx-auto open-sans">
         <h1 className="font-medium ">fuddie .</h1>
         <ul className="flex items-center justify-center gap-10">
-          <li className="font-medium">Home</li>
+          <li className="font-medium open-sans">Home</li>
           <li className="font-medium">About</li>
           <li className="font-medium">Download</li>
           <li className="font-medium">Admin</li>
